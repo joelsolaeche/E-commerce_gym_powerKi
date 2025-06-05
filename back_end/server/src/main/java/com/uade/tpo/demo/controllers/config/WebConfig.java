eb45b0c7741ddf568b8789b8cf36de05d2085bc2
Gabriel Kraus <gabrielalvarezkraus@gmail.com>
2025-06-05 15:28:07 -0300

    correcciones

--- back_end/server/src/main/java/com/uade/tpo/demo/controllers/config/WebConfig.java
+++ back_end/server/src/main/java/com/uade/tpo/demo/controllers/config/WebConfig.java
@@ -23,11 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("classpath:/static/uploads/")
-                .addResourceLocations("file:/home/tomas/StickyPalooza-Back/server/src/main/resources/static/uploads/")
-                .addResourceLocations("file:/home/juanchi/workspace/StickyPalooza-Back/server/src/main/resources/static/uploads/");
+                .addResourceLocations("classpath:/static/uploads/");
                 
-                // PROFE SI ESTAS LEYENDO ESTO POR FAVOR PERDON YA SE QUE ESTA MAL EL MANEJO DE IMAGENES 
-                // CON UNA RUTA ASI PERO ES MOMENTANEO, VA A SER ARREGLADO SI O SI GRACIAS LE MANDO UN SALUDO!!
             }
 }
